53ab45bb09a6e8df58f61c03fb82fec89d0779ce
1105-Bunachita-Castor <144702114+1105-Bunachita-Castor@users.noreply.github.com>
2025-04-27 20:41:16 -0700

    First Commit
    First Commit with code for serial activation

---
+++ Final_Project.ino
@@ -0,0 +1,38 @@
+// Group 64
+// Group Members: Castor Bunachita 
+// Date: 04/27/25
+// Title: Final Project Water Cooler
+
+ #define RDA 0x80
+ #define TBE 0x20  
+ volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+ volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+ volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+ volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
+ volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
+
+void setup() {
+ U0init(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void U0init(unsigned long U0baud)
+{
+//  Students are responsible for understanding
+//  this initialization code for the ATmega2560 USART0
+//  and will be expected to be able to intialize
+//  the USART in differrent modes.
+//
+ unsigned long FCPU = 16000000;
+ unsigned int tbaud;
+ tbaud = (FCPU / 16 / U0baud - 1);
+ // Same as (FCPU / (16 * U0baud)) - 1;
+ *myUCSR0A = 0x20;
+ *myUCSR0B = 0x18;
+ *myUCSR0C = 0x06;
+ *myUBRR0  = tbaud;
+}
